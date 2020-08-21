e72ba09eaf6cf0b3ad9ec813fe010a66f6efb46a
zLounyM <prusa.developer@gmail.com>
2020-08-21 11:19:02 +0200

    fix test and change configuration to use dev env as default

--- src/test/java/cz/zlounym/shareit/ShareItApplicationTests.java
+++ src/test/java/cz/zlounym/shareit/ShareItApplicationTests.java
@@ -3,9 +3,11 @@ package cz.zlounym.shareit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("local")
 @SpringBootTest
 public class ShareItApplicationTests {
 
