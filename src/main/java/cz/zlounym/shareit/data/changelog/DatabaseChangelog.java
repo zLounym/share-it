6c4097f59724a95c1f992b0821519c57e28a5505
zLounyM <prusa.developer@gmail.com>
2020-08-21 12:11:01 +0200

    add mongobee config and fix mongo configuration

---
+++ src/main/java/cz/zlounym/shareit/data/changelog/DatabaseChangelog.java
@@ -0,0 +1,13 @@
+package cz.zlounym.shareit.data.changelog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.mongobee.changeset.ChangeLog;
+
+@ChangeLog
+public class DatabaseChangelog {
+
+    private static final Logger log = LoggerFactory.getLogger(DatabaseChangelog.class);
+
+}
